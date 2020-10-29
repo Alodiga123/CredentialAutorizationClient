f50490465ecf152150698a9da398c54e212cfc63
moises <moisegrat12@gmail.com>
2020-10-29 11:27:24 -0400

    Creacion de libreria de dispersion y adelanto de limite

---
+++ CredentialAutorizationClient/src/com/alodiga/autorization/utils/Constants.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package com.alodiga.autorization.utils;
+
+/**
+ * @author jonathanxuya
+ *
+ */
+public class Constants {
+
+    public static String PASSWORD_TEST = "c6DePc/9BRd2l5iL8vlcwgKfbQCOJbvhzsmFBi+iDagfTRTOnmyNmAvw0Kn5L60yG5QMxbFNdtL4wZdqe0rAS6C1duqmznK/UJN5XVPcMh6lIRIo6eAmzQ7xAFIC0uPd79RQerN+uvWKQFWXcWEces+rlR6SRtCZ+dXqGXWobEA=";
+    public static String USER_TEST = "usuarioWStest";
+    public static String CANAL = "Mobilcash";
+    public static String IP_NUMBER_TEST = "192.168.3.20";
+    public static Long NUMBER_ENTRY_MODE = 1L;
+    public static Long CURRENCY_CODE = 854L;
+    //public static Long CURRENCY_CODE = 484L;
+
+    public static String URL_TEST = "https://10.70.10.85:8000/Autorizacion";
+    public static String URL_TEST_WS = "https://10.70.10.85:8000/Autorizacion?wsdl";
+
+    //DISPERTION
+    public static Long NUMBER_TRANSACTION_TYPE_DISPERTION = 35L;
+    public static Long NUMBER_SUB_TRANSACTION_TYPE_DISPERTION = 1L;
+    public static String CONCEPT_TRANSFER_DISPERTION = "TRANSFERENCIA DE ABONO";
+    
+    //LIMIT ADVANCE
+    public static Long NUMBER_TRANSACTION_TYPE_LIMIT_ADVANCE = 35L;
+    public static Long NUMBER_SUB_TRANSACTION_TYPE_LIMIT_ADVANCE = 3L;
+    public static String CONCEPT_TRANSFER_LIMIT_ADVANCE = "TRANSFERENCIA DE RETIRO";
+
+}
